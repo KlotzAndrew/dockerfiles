2607d8ec4e50d39cc93f8faa5ddf39df8d044ac6
Andrew Klotz <agc.klotz@gmail.com>
2018-07-20 04:13:42 +0000

    awscli

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 docker login -u klotzandrew
 
@@ -8,3 +8,6 @@ docker build -f ngrok/Dockerfile -t ngrok ngrok/
 docker tag ngrok klotzandrew/ngrok
 docker push klotzandrew/ngrok
 
+docker build -f awscli/Dockerfile -t awscli awscli/
+docker tag awscli klotzandrew/awscli
+docker push klotzandrew/awscli
