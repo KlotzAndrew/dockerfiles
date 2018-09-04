39083cfdc597f5cfe1cd66851eea45a0f81bbd0b
Andrew Klotz <agc.klotz@gmail.com>
2018-09-04 22:45:30 +0000

    shellcheck & build script

--- build.sh
+++ build.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-docker login -u klotzandrew
+# docker login -u klotzandrew
 
-docker build -f ngrok/Dockerfile -t ngrok ngrok/
-docker tag ngrok klotzandrew/ngrok
-docker push klotzandrew/ngrok
+build() {
+	f=$1
+	image_path=${f%Dockerfile}
+	image=${image_path%%\/*}
 
-docker build -f awscli/Dockerfile -t awscli awscli/
-docker tag awscli klotzandrew/awscli
-docker push klotzandrew/awscli
+	repo=klotzandrew
+	tag=latest
+
+	build_slug="$repo/$image:$tag"
+
+	echo "Building start: $build_slug"
+	docker build --rm --force-rm -t "$build_slug" "$image_path"
+	docker push "$build_slug"
+
+	echo "====="
+	echo "Building done: $build_slug"
+	echo "====="
+}
+
+containers=$1
+if [ -z "$containers" ]; then
+	containers=. # build all
+fi
+
+files=$(find . -name 'Dockerfile' -type f | sed 's|./||' | sort | grep $containers)
+
+for f in $files; do
+	build "$f"
+done
