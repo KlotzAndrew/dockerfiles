b85688a6b3c2406c2d35fa911f30fce6f282bed9
Andrew Klotz <agc.klotz@gmail.com>
2020-07-11 16:49:45 +0000

    prefer explicit escaping

--- dummy-webserver/serve.sh
+++ dummy-webserver/serve.sh
@@ -3,4 +3,4 @@
 port=${PORT:-3000}
 
 echo "starting on port $port"
-while true; do printf "HTTP/1.1 200 OK\n\n ready: %s\n" "$(date)" | nc -l -p "$port" -q 1; done
+while true; do printf "HTTP/1.1 200 OK\\n\\n ready: %s\\n" "$(date)" | nc -l -p "$port" -q 1; done
